7ebda8a7827cdf602ef2d5b1002acb0a3cadf001
Yunus Ali Siddiqui <yunusalisiddiqui90@gmail.com>
2024-01-03 18:30:51 +0530

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import '../styles/modern-normalizer.css';
+import '../styles/style.css';
+import '../styles/header.css';
+import '../styles/mobile-nav.css';
+import '../styles/front.css';
+import '../styles/util.css';
+
+
+
+import mobileNav from './utils/mobile-nav';
+import darkMode from './utils/dark-mode';
+
+
+mobileNav();
+darkMode();
